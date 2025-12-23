60131d742ee35a888bda1953cbbf1d9b215aa213
yashhh-dev <indoyash27@gmail.com>
2025-12-23 21:24:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+"""Central configuration for credentials.
+
+Change UID/PW ONLY here.
+"""
+
+# Free Fire guest/account credentials
+UID = "4346202329"
+PW  = "4A200451676C4629E5D90F60B6113E73B650C94D7C20CBCA5F69978991648D12"
+
+# Bot UID used in packets (keep same as UID unless you know otherwise)
+BOT_UID = UID
